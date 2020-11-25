b685cfb6fd7b6504881787f5279a9b6aafb4b7e8
Marcus Ma <32466398+mamarcus64@users.noreply.github.com>
2020-11-24 16:14:22 -0800

    initial commit

---
+++ src/TEst.java
@@ -0,0 +1,8 @@
+public class TEst {
+
+    public static void main(String[] args) {
+        String ko = "ㅏ두ㅗ디ㅣㅐㅑㄴ둘미룬랴눌";
+        for (char c : ko.toCharArray())
+            System.out.println(c);
+    }
+}
